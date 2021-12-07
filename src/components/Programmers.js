c169f969b2f3c29a10c32e543bf0f27e9b844fdc
MNoble2014 <michealsimpson2013@gmail.com>
2021-12-07 08:59:12 -0500

    programmers.js .map updated to programmers

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -52,9 +52,9 @@ export default function Programmers() {
           /* Nasty bug! We should map over a slice of state, instead of 'listOfAwesome'.
           We might think: "it works, though!" But if the list of programmers is not state,
           we could never add or edit programmers in the future. The list would be a static thing." */
-          listOfAwesome.map(dev =>
+          programmers.map(dev =>
             <div className='programmer' key={dev.id}>
-              {dev.name} <button onClick={() => { /* in here set the featured id to be dev.id */ }}>Feature</button>
+              {dev.name} <button onClick={() => { setFeatured(dev.id); }}>Feature</button>
             </div>
           )
         }
